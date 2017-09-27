b338bd4006e570bf20e8e563718a3010ee24b08a
ChilliHsu <b03705043@ntu.edu.tw>
2017-09-27 23:01:23 +0800

    "我上傳的音檔"  ListView is OK ，但點下去的結果還沒做

--- app/src/main/java/com/example/user/logintest/MyuploadMusic.java
+++ app/src/main/java/com/example/user/logintest/MyuploadMusic.java
@@ -2,12 +2,20 @@ package com.example.user.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class MyuploadMusic extends AppCompatActivity {
     String post="";
+    private ListView listView;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,6 @@ public class MyuploadMusic extends AppCompatActivity {
         Bundle bundleID =this.getIntent().getExtras();
 
         String account = bundleID.getString("AccountID");
-        TextView audiolist = (TextView)findViewById(R.id.audiolist);
         //ID:帳號
         //連接php，將相關的音檔載下來
         String phpURL = "http://140.112.107.125:47155/html/myMusic.php" ;
@@ -32,7 +39,24 @@ public class MyuploadMusic extends AppCompatActivity {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        audiolist.setText(post);
+        final String[] AudioList = post.split(";");
+        final String[] NameList = new String[AudioList.length-1];
+        for(int i=0;i<AudioList.length-1;i++){
+            if(AudioList[i].lastIndexOf("intro:")>=0){
+                NameList[i]=AudioList[i].substring(AudioList[i].indexOf("intro:")+6);
+            }
+        }
+        listView = (ListView)findViewById(R.id.list_view);
+        listAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,NameList);
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //這裡應該要顯示點下去應該要出現的頁面
+                Toast.makeText(getApplicationContext(), "你上傳的音檔名稱是:  " + NameList[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
 
     }
