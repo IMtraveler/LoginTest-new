18d7ea42b835afe8745bcf1338078f1dd176cbec
ChilliHsu <b03705043@ntu.edu.tw>
2017-10-24 00:31:27 +0800

    ListSpotView

---
+++ app/src/main/java/com/example/user/logintest/SpotView.java
@@ -0,0 +1,13 @@
+package com.example.user.logintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpotView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spot_view);
+    }
+}
