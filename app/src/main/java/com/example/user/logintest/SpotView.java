6a0af7465585455446951151be722ff144a19a86
ChilliHsu <b03705043@ntu.edu.tw>
2017-10-25 01:08:52 +0800

    spotslist

--- app/src/main/java/com/example/user/logintest/SpotView.java
+++ app/src/main/java/com/example/user/logintest/SpotView.java
@@ -2,6 +2,7 @@ package com.example.user.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
@@ -12,9 +13,12 @@ public class SpotView extends AppCompatActivity {
     LocationsDatabase myDatabase;
     private ArrayList<Locations> locationArrayList;
     private String[] locations ;
-    private ArrayAdapter<String> adapter;
     ListView listView;
     private ArrayAdapter<String> listAdapter;
+    ArrayAdapter mSearchAdapter;
+    ArrayList<String> mSearchList = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private AutoCompleteTextView searchbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,12 @@ public class SpotView extends AppCompatActivity {
         listView = (ListView)findViewById(R.id.spot_list);
         listAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,locations);
         listView.setAdapter(listAdapter);
-
+       // adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,locations);
+        searchbar = (AutoCompleteTextView) findViewById(R.id.seachbar);
+        searchbar.setThreshold(1);
+        searchbar.setAdapter(listAdapter);
     }
+
+
+
 }
