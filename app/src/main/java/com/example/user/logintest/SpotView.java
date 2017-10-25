0f25c97e551edaf7bcc26712f2ed8b27c2a90e57
ChilliHsu <b03705043@ntu.edu.tw>
2017-10-25 09:34:02 +0800

    spotlist is OK, only search need to be modified

--- app/src/main/java/com/example/user/logintest/SpotView.java
+++ app/src/main/java/com/example/user/logintest/SpotView.java
@@ -1,24 +1,38 @@
 package com.example.user.logintest;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class SpotView extends AppCompatActivity {
     LocationsDatabase myDatabase;
     private ArrayList<Locations> locationArrayList;
     private String[] locations ;
     ListView listView;
+    SearchView searchView;
     private ArrayAdapter<String> listAdapter;
-    ArrayAdapter mSearchAdapter;
-    ArrayList<String> mSearchList = new ArrayList<>();
-    private ArrayAdapter<String> adapter;
-    private AutoCompleteTextView searchbar;
+    Bundle bundle = new Bundle();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +45,94 @@ public class SpotView extends AppCompatActivity {
             locations[i]=locationArrayList.get(i).name;
         }
         listView = (ListView)findViewById(R.id.spot_list);
-        listAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,locations);
+        searchView = (SearchView)findViewById(R.id.SearchView);
+        listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,locations);
         listView.setAdapter(listAdapter);
-       // adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,locations);
-        searchbar = (AutoCompleteTextView) findViewById(R.id.seachbar);
-        searchbar.setThreshold(1);
-        searchbar.setAdapter(listAdapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //locationArrayList=myDatabase.getsearch(query);
+                //for(int i=0 ;i<locationArrayList.size();i++){
+                //    locations[i]=locationArrayList.get(i).name;
+                //}
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                listAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String lat = String.valueOf(locationArrayList.get(position).lat) ;
+                String lng = String.valueOf(locationArrayList.get(position).lng) ;
+                Toast.makeText(getApplicationContext(),"lat: "+lat+" lng: "+lng,Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(SpotView.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog, null);
+                Button mLogin = (Button) mView.findViewById(R.id.buttona);
+                TextView tv_intro = (TextView)mView.findViewById(R.id.tv_name);
+                ImageView imageView = (ImageView)mView.findViewById(R.id.imageView);
+                String phpURL = "http://140.112.107.125:47155/html/test.php" ;
+
+                bundle.putString("lat", lat);
+                bundle.putString("lng", lng);
+                String post = "";
+                try {
+                    post = new MyAsyncTask().execute(lat, lng, phpURL).get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+                bundle.putString("post", post);
+
+                if (post.length() > 10) {
+                    int begName = post.indexOf("name:");
+                    int endName = post.indexOf("altHead_name");
+                    String name = post.substring(begName + 5, endName - 1);
+                    tv_intro.setText(name);
+                    bundle.putString("name", name);
+                    int begImg = post.indexOf("image");
+                    int endIndex = post.length();
+
+                    if ((begImg + 6) >= endIndex - 5) {
+                        Picasso.with(getBaseContext()).load("http://140.112.107.125:47155/html/uploaded/null.png").into(imageView);
+                        bundle.putString("imgURL", "http://140.112.107.125:47155/html/uploaded/null.png");
+                    } else {
+                        String imgURL = "http:" + post.substring(begImg + 6, endIndex);
+                        imgURL = imgURL.replaceAll(" ", "");
+                        Picasso.with(getBaseContext()).load(imgURL).into(imageView);
+                        bundle.putString("imgURL", imgURL);
+                    }
+                }
+                else{
+                    bundle.putString("imgURL","http://140.112.107.125:47155/html/uploaded/null.png");
+                    tv_intro.setText("no data");
+                    bundle.putString("name", "no data");
+                }
+                mLogin.setOnClickListener(new View.OnClickListener()  {
+                                              @Override
+                                              public void onClick(View view){
+                                                  Intent intent = new Intent();
+                                                  intent.setClass(SpotView.this, AttractionsActivity.class);
+                                                  intent.putExtras(bundle);
+                                                  startActivity(intent);
+                                              }
+
+                                          }
+                );
+                mBuilder.setView(mView);
+                AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+            }
+        });
+
     }
 
 
