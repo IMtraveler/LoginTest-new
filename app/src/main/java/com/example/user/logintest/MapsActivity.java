b7a7b96fd943364202a3f8f064480095efd7472c
puravida112 <doris12458@gmail.com>
2017-12-02 17:48:48 +0800

    fix crash problem when uploading chinese file name but still can't upload / stop users from audiopage when there's no audio

--- app/src/main/java/com/example/user/logintest/MapsActivity.java
+++ app/src/main/java/com/example/user/logintest/MapsActivity.java
@@ -1,28 +1,16 @@
 package com.example.user.logintest;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.BitmapFactory;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.os.AsyncTask;
 import android.os.Build;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.StringDef;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,11 +22,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Intent ;
-import android.graphics.Bitmap;
 
 
 import com.google.android.gms.common.ConnectionResult;
@@ -50,21 +36,13 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InvalidObjectException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import static com.example.user.logintest.Downloads.mContext;
-import static com.example.user.logintest.MySQLConnection.Post;
 import com.squareup.picasso.Picasso ;
 
 
@@ -281,7 +259,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 final String locating = marker.getPosition().toString() ;
                 String lat = String.valueOf(marker.getPosition().latitude) ;
                 String lng = String.valueOf(marker.getPosition().longitude) ;
-                String phpURL = "http://140.112.107.125:47155/html/test.php" ;
+                String phpURL = "http://140.112.107.125:47155/html/getdata.php" ;
                 //final String post = Post(lat, lng);
                 bundle.putString("lat", lat);
                 bundle.putString("lng", lng);
@@ -312,7 +290,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     bundle.putString("name", name);
                     int begImg = post.indexOf("image");
                     int endIndex = post.indexOf("classified")-1;
-
+                    String audioNumStr =post.substring(post.indexOf("audioNum:")+10, post.length());
+                    int audioNum =  Integer.valueOf(audioNumStr.trim().replaceAll("/n ", ""));
+                    bundle.putInt("audioNum", audioNum);
                     if ((begImg + 6) >= endIndex - 5) {
                         //Picasso.with(getBaseContext()).load("http://140.112.107.125:47155/html/uploaded/null.png").into(imageView);
                         bundle.putString("imgURL", "http://140.112.107.125:47155/html/uploaded/null.png");
